6678d10e0d016e71091fe8678998959ba2c09730
Jake Price <git@jakeprice.dev>
2022-02-18 20:38:23 +0000

    Add TODO

--- monitors_arrange_lid_down.sh
+++ monitors_arrange_lid_down.sh
@@ -12,6 +12,7 @@ RIGHT="DP-2-1-1-8"
 # Set primary:
 xrandr --output $MIDDLE --primary
 
+# TODO only run _if_ other monitors detected, otherwise laptop screen goes off when your only using the laptop!
 # Set screen arrangement:
 xrandr --output $LAPTOP --off
 xrandr --output $LEFT --left-of $MIDDLE
