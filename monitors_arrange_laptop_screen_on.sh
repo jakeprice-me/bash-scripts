4077ef6eabb234f8074f03171dcddc089327838c
Jake Price <git@jakeprice.dev>
2022-02-19 16:19:34 +0000

    Rename + Renable laptop screen if off

--- monitors_arrange_laptop_screen_on.sh
+++ monitors_arrange_laptop_screen_on.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # *****************************************************************************
-# Arrange monitors when laptop lid is closed
+# Arrange monitors with laptop screen on.
 # *****************************************************************************
 
 # Screen names:
@@ -12,10 +12,8 @@ RIGHT="DP-2-1-1-8"
 # Set primary:
 xrandr --output $MIDDLE --primary
 
-# TODO only run _if_ other monitors detected, otherwise laptop screen goes off when your only using the laptop!
 # Set screen arrangement:
-xrandr --output $LAPTOP --off
-xrandr --output $LEFT --left-of $MIDDLE
+xrandr --output $LAPTOP --auto --below $LEFT
 xrandr --output $MIDDLE --right-of $LEFT
 xrandr --output $RIGHT --right-of $MIDDLE
 
