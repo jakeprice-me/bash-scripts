3948f9a9b16aa07f1262608b20b6d219dbd51a8a
Jake Price <git@jakeprice.dev>
2022-03-14 21:53:38 +0000

    Renable 9th camera

--- camera_mosaic.sh
+++ camera_mosaic.sh
@@ -25,15 +25,16 @@ source "$HOME"/.cctv
 # Create mosaic:
 ffmpeg \
     -i rtsp://"$USERNAME":"$PASSWORD"@"$SUBNET".215/stream2 \
-    -i rtsp://"$USERNAME":"$PASSWORD"@"$SUBNET".217/stream2 \
-    -i rtsp://"$USERNAME":"$PASSWORD"@"$SUBNET".213/stream2 \
-    -i rtsp://"$USERNAME":"$PASSWORD"@"$SUBNET".210/stream2 \
-    -i rtsp://"$USERNAME":"$PASSWORD"@"$SUBNET".218/stream2 \
-    -i rtsp://"$USERNAME":"$PASSWORD"@"$SUBNET".214/stream2 \
     -i rtsp://"$USERNAME":"$PASSWORD"@"$SUBNET".212/stream2 \
     -i rtsp://"$USERNAME":"$PASSWORD"@"$SUBNET".216/stream2 \
+    -i rtsp://"$USERNAME":"$PASSWORD"@"$SUBNET".210/stream2 \
+    -i rtsp://"$USERNAME":"$PASSWORD"@"$SUBNET".214/stream2 \
+    -i rtsp://"$USERNAME":"$PASSWORD"@"$SUBNET".217/stream2 \
+    -i rtsp://"$USERNAME":"$PASSWORD"@"$SUBNET".211/stream2 \
+    -i rtsp://"$USERNAME":"$PASSWORD"@"$SUBNET".213/stream2 \
+    -i rtsp://"$USERNAME":"$PASSWORD"@"$SUBNET".218/stream2 \
     -filter_complex "
-        [0:v][1:v][2:v][3:v][4:v][5:v][6:v][7:v]xstack=inputs=8:layout=0_0|0_h0|0_h0+h1|w0_0|w0_h0|w0_h0+h1|w0+w3_0|w0+w3_h0:fill=black[v]
+        [0:v][1:v][2:v][3:v][4:v][5:v][6:v][7:v][8:v]xstack=inputs=9:layout=0_0|0_h0|0_h0+h1|w0_0|w0_h0|w0_h0+h1|w0+w3_0|w0+w3_h0|w0+w3_h0+h1:fill=black[v]
 	" \
     -map "[v]" \
 	-c:v libx264 -preset superfast -crf 18 -f matroska - | ffplay -
