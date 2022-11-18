91a3a4accba6c4377086f0c938c0812456a4bf92
Jake Price <git@jakeprice.dev>
2022-11-18 15:00:55 +0000

    Add updatedAt field and change date/time format

--- github_issues_table.sh
+++ github_issues_table.sh
@@ -12,12 +12,12 @@
 # *****************************************************************************
 
 # Set variables:
-FLAGS="--assignee @me --state open --json number,createdAt,title,state,labels,url"
+FLAGS="--assignee @me --state open --json number,createdAt,updatedAt,title,state,labels,url"
 REPO_1=""
 REPO_2=""
 
 # Collect and display issues:
 ( gh issue list --repo "$REPO_1" $FLAGS ; \
     gh issue list --repo "$REPO_2" $FLAGS ) \
-    | jq -cs 'add | sort_by(.createdAt) | .[] | {Number: .number, Created: .createdAt | fromdate | strftime("%Y-%m-%d"), Title: .title, Labels: [.labels[].name]|join(", "), Link: .url}' | jtbl -t
+    | jq -cs 'add | sort_by(.createdAt) | .[] | {Number: .number, Created: .createdAt | fromdate | strftime("%Y-%m-%d %H:%M"), Updated: .updatedAt | fromdate | strftime("%Y-%m-%d %H:%M"), Title: .title, Labels: [.labels[].name]|join(", "), Link: .url}' | jtbl -t
  
